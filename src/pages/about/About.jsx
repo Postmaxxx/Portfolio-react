29d99c1fb070f932a597217eff8c35aa9682b0b8
Maxim Postnikov <max@postnikov.dev>
2022-11-06 17:43:33 +0300

    recreating nav

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import './about.scss'
+import photo1 from '../../assets/img/photo-1.jpg';
 
 export default function Aboutpage() {
-    console.log('about' );
     return (
-        <>
-            <button style={{position: 'absolute', left: '300px'}}
-                >
-                About
-            </button>
-        </>
+        <div className="page_about">
+            <div className="page-container">
+                <section class='about_me'>
+                    <h2>About me<em>About me</em></h2>
+                    <div className="about__container">
+                        <div className="img-container">
+                            <img src={photo1} alt="My photo" />
+                        </div>
+                        <div className="descr">
+                            <h3>I am <em>Postnikov Max</em></h3>
+                            <p>I am a frontend web developer. I can provide clean code and pixel 
+                                perfect design. I also make website more & more interactive with 
+                                web animations.</p>
+                            <ul>
+                                <li><b>Full Name</b>Max Postnikov</li>
+                                <li><b>Age</b>39</li>
+                                <li><b>Nationality</b>Russian</li>
+                                <li><b>Languages</b>Russian, English</li>
+                                <li><b>Adress</b>Alanya, Turkey</li>
+                                <li><b>Freelance</b>Available</li>
+                            </ul>
+                            <a href="#">Download cv</a>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+
+        </div>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,25 @@
 import './home.scss'
-import React, { useEffect } from "react";
-import { connect } from 'react-redux'
-import * as actions from '../../assets/redux/actions'
-import ThemeSwitcher from '../../components/theme_switcher/ThemeSwitcher';
+import React from "react";
+import logoGit from '../../assets/img/svg/logo-git.svg';
 
 const Homepage = ( props ) => {
     return (
-        <div className="page_home">
-            <h1>Hi, I am <em>Max Postnikov</em></h1>
-            <p>I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.</p>
+        <div className="page-container">
+            <div className="page_home">
+                <h1>Hi, I am <em>Max Postnikov</em></h1>
+                <p>I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.</p>
+                <div className="socials">
+                    <a href="#">
+                        <img src={logoGit} alt="GitHub" />
+                    </a>
+                    <a href="#">
+                        <img src={logoGit} alt="GitHub" />
+                    </a>
+                    <a href="#">
+                        <img src={logoGit} alt="GitHub" />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
